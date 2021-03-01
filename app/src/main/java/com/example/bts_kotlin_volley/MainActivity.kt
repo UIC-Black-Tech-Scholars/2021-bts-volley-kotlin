a604d0e973c572d66a0e207cfabc7446352b00fd
Lucas Oskorep <lucas.oskorep@gmail.com>
2021-03-01 16:27:46 -0600

    adding base of volley repo

--- app/src/main/java/com/example/bts_kotlin_volley/MainActivity.kt
+++ app/src/main/java/com/example/bts_kotlin_volley/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.bts_kotlin_volley
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.Volley
 
 class MainActivity : AppCompatActivity() {
+
+    private var requestQueue: RequestQueue? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        val image = findViewById<ImageView>(R.id.imageView)
+        val firstText = findViewById<TextView>(R.id.textView)
+        val secondText = findViewById<TextView>(R.id.textView2)
+
+        requestQueue = Volley.newRequestQueue(this)
+
+        button.setOnClickListener { makeWebRequests() }
+
+    }
+
+    private fun makeWebRequests(){
+
+        val textUrl = "https://pastebin.com/raw/HNGezyyW"
+
+
+        val ImageUrl = "https://pastebin.com/raw/AjEDaQ4V"
+
     }
 }
